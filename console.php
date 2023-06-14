ec046598fba26b39cac9d9d551d5a506f391becd
Owen <owen.herbert@hotmail.com>
2023-06-14 11:01:39 +1000

    Add console helper class

---
+++ console.php
@@ -0,0 +1,9 @@
+<?php
+
+    class Console {
+        
+        // echo the message with a new line character for readability
+        public static function output($message) {
+            echo $message . PHP_EOL;
+        }
+    }
